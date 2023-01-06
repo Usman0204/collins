829856061da779d8b755c7e18471d1ef647579a3
m-jawa-d <chudhryjawad@gmail.com>
2023-01-06 18:58:20 +0500

    fixes

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,10 +1,10 @@
 let Environment = {
   /////////-----Testnet------////////////////
-  token:'0xadc73096073c83b8A525455ad649eebE3a38Ba74',
-  staking: '0xeEEe6a1083d87ab2c76757A69Ab1641FAf3A5d8c',
+  // token:'0xadc73096073c83b8A525455ad649eebE3a38Ba74',
+  // staking: '0xeEEe6a1083d87ab2c76757A69Ab1641FAf3A5d8c',
    /////////-----Mainet------////////////////
-  // token:'0x31491c35C094A0336f4859Dd94aB9466709deC45',
-  // staking: '0x29fcc258b11c5e70928040590f01eD0d7eb167E3'
+  token:'0x31491c35C094A0336f4859Dd94aB9466709deC45',
+  staking: '0x29fcc258b11c5e70928040590f01eD0d7eb167E3'
 };
 
 export default Environment;
