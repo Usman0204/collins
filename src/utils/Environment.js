4b4640a45d487195eca6b6febd65768a8640c6e9
m-jawa-d <chudhryjawad@gmail.com>
2023-01-09 16:22:36 +0500

    mainnet fixes

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -3,12 +3,12 @@ let Environment = {
   // token:'0xadc73096073c83b8A525455ad649eebE3a38Ba74',
   // staking: '0xeEEe6a1083d87ab2c76757A69Ab1641FAf3A5d8c',
    /////////-----Mainet------/////////////////////////////////////////////////////////////
-  // token:'0x31491c35C094A0336f4859Dd94aB9466709deC45',
-  // staking: '0x29fcc258b11c5e70928040590f01eD0d7eb167E3'
+  token:'0x31491c35C094A0336f4859Dd94aB9466709deC45',
+  staking: '0x29fcc258b11c5e70928040590f01eD0d7eb167E3'
 
   ////////////////////moday token and staking///////////////////////////////////////////
-  staking:'0x62524379D10f98ebfB452995966DD1CA58570eAa',
-  token:'0x99366A7388c9E5baD3A0835fA6361917dc0d8448'
+  // staking:'0x62524379D10f98ebfB452995966DD1CA58570eAa',
+  // token:'0x99366A7388c9E5baD3A0835fA6361917dc0d8448'
 };
 
 export default Environment;
